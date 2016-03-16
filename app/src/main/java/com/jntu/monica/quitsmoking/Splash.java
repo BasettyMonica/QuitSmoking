464c0bd109eafe7044d99421cfe4ea07006de163
BasettyMonica <basettymonica98@gmail.com>
2016-03-16 21:50:25 +0530

    Added progress bar to the splash screen

--- app/src/main/java/com/jntu/monica/quitsmoking/Splash.java
+++ app/src/main/java/com/jntu/monica/quitsmoking/Splash.java
@@ -2,12 +2,18 @@ package com.jntu.monica.quitsmoking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class Splash extends AppCompatActivity {
+
+    ProgressBar splashBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        splashBar = (ProgressBar) findViewById(R.id.SplashProgress);
+        splashBar.setVisibility(View.GONE);
     }
 }
