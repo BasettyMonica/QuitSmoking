de6d5b5c02a153426c29ffd0b6d7d3400fba98ae
basetty monica <basetty monica>
2016-03-16 21:14:19 +0530

    Initialized QuitSmoking App

---
+++ app/src/main/java/com/jntu/monica/quitsmoking/Splash.java
@@ -0,0 +1,13 @@
+package com.jntu.monica.quitsmoking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
